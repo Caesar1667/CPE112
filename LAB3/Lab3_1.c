54fe4b4408c939104b60672ab8eaaa23250eeacc
Pansa Intawong <pansaintawong1234567@gmail.com>
2024-02-02 04:39:08 +0700

    made minor changes to 3_1 and update first feature of 3_2

--- LAB3/Lab3_1.c
+++ LAB3/Lab3_1.c
@@ -1,60 +1,79 @@
+//Pansa Intawong 66070503474
+//Insert Linked List
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node{
+typedef struct node
+{
     int data;
     struct node *next;
 } node_t;
 
-void insertBegin(node_t **head, int val){
+struct Node {
+	int data;
+	struct Node* next;
+};
+
+void insertBegin(node_t **head, int val)
+{
     node_t *new_node;
-    new_node = (node_t*)malloc(sizeof(node_t));
+    new_node = (node_t *)malloc(sizeof(node_t));
 
     new_node->data = val;
     new_node->next = *head;
     *head = new_node;
 }
 
-void insertEnd(node_t **head, int val){
-    node_t *new_node;
-    new_node = (node_t*)malloc(sizeof(node_t));
+void insertEnd(node_t **head, int val)
+{
+    node_t *new_node = (node_t *)malloc(sizeof(node_t));
+    new_node->data = val;
+
     node_t *last = *head;
 
-    new_node->data = val;
     new_node->next = NULL;
 
-    if(*head == NULL){
+    if (*head == NULL)
+    {
         *head = new_node;
         return;
     }
 
-    while(last->next != NULL){
+    while (last->next != NULL)
+    {
         last = last->next;
     }
-
     last->next = new_node;
-    return;
 }
 
-int main(){
-    int size;
-    scanf("%d", &size);
+int main()
+{
+    int size, option;
+    scanf("%d\n%d", &size, &option);
     node_t *head = NULL;
-    head = (node_t*)malloc(sizeof(node_t));
-    if(head == NULL){
-        return 1;
+        for (int i = 0; i < size; i++)
+        {
+            int temp;
+            scanf("%d", &temp);
+            insertEnd(&head, temp);
+            // if(option == 1){
+            //     insertBegin(&head, temp);
+            // }else if(option == 2){
+            //     insertEnd(&head, temp);
+            // }else{
+            //     printf("Invalid");
+            //     return 0;
+            // }
+        }
+    
+    if(size == 0){
+        printf("Invalid");
+        return 0;
     }
-    for(int i = 0; i < size; i++){
-        int temp;
-        scanf("%d", &temp);
-        // insertBegin(&head, temp);
-        insertEnd(&head, temp);
+    while(head != NULL){
+        printf("%d ", head->data);
+        head = head->next;
     }
-    node_t *current = head;
 
-    for(int i = 0; i < size; i++){
-        printf("%d ", current->data);
-        current = current->next;
-    }
-
+    return 0;
 }
--- LAB3/Lab3_2.c
+++ LAB3/Lab3_2.c
@@ -16,7 +16,7 @@ void append(node_t **head, int val){
     new_node->next = NULL;
 
     if(*head == NULL){
-        head = new_node;
+        *head = new_node;
         return;
     }
 
@@ -35,10 +35,32 @@ bool search(node_t *head, int val){
     }
     return search(head->next, val);
 }
-int insertAfter(node_t **head, int val){
-    node_t new_node = (node_t*)malloc(sizeof(node_t))
-    if(search(head, val)){
-        
+void insertAfter(node_t **head, int ref_val, int ins_val){
+    node_t *new_node = (node_t *)malloc(sizeof(node_t));
+    new_node->data = ins_val;
+
+    node_t *last = *head;
+
+    // new_node->next = NULL;
+
+    if (*head == NULL)
+    {
+        *head = new_node;
+        return;
+    }
+
+    while (last->data != ref_val && last->next != NULL)
+    {
+        last = last->next;
+    }
+    new_node->next = last->next;
+    last->next = new_node;
+}
+
+void printList(node_t *head){
+    while(head != NULL){
+        printf("%d ", head->data);
+        head = head->next;
     }
 }
 
@@ -51,10 +73,16 @@ int main(){
         scanf("%d", &temp);
         append(&head, temp);
     }
-    char mode;
-    do{
-        if(mode == 'A'){
-
-        }
-    }while(mode != 'E');
+    char mode[1];
+    int ref_val = 2, ins_val = 4;
+    // scanf("%c %d %d", mode, ref_val, ins_val);
+    insertAfter(&head, ref_val, ins_val);
+    printList(head);
+    // do{
+    //     scanf("%c %d %d", mode, ref_val, ins_val);
+    //     if(mode[0] == 'A'){
+    //         insertAfter(&head, ref_val, ins_val);
+    //     }
+    // }while(mode != 'E');
+    return 0;
 }
