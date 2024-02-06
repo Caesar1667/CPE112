7aa9154274a65ef11b4ddc540fb22810890b8402
Pansa Intawong <pansaintawong1234567@gmail.com>
2024-02-06 21:06:56 +0700

    Update asdas.c

--- test/asdas.c
+++ test/asdas.c
@@ -1,142 +1,146 @@
-// 66070503421 Nutthawut Jaroenchokwittaya
-// Delete first last or same value node and print remaining
+// Phuwasin Kriangdechachai 66070503478
 #include <stdio.h>
-#include<stdlib.h>
-// Define the structure for a linked list node
+#include <stdlib.h>
+#include <string.h>
+
 typedef struct node {
     int data;
-    struct node* next;
+    struct node *next;
+    struct node *prev;
 } node;
-void insert_back(node** start, int data) {
-    node* newnode, *ptr;
-    newnode = (node*)malloc(sizeof(node));
-    newnode->data = data;
-    newnode->next = NULL;
 
-    if (*start == NULL) {
-        // If the list is empty, the new node becomes the start
-        *start = newnode;
-    } else {
-        // Traverse to the end of the list and add the new node
-        ptr = *start;
-        while (ptr->next != NULL) {
-            ptr = ptr->next;
-        }
-        ptr->next = newnode;
+void Forward(node *head) {
+    node *new_node = head;
+    while (new_node != NULL) {
+        printf("%d ", new_node->data);
+        new_node = new_node->next;
     }
 }
-void deletefirst(node** start){
-  node* ptr;
-  if(*start==NULL){
-    return;
-  }
-  ptr=*start;
-  *start=(*start)->next;
-  free(ptr);
-}
-void deletelast(node** start){
-  node* ptr,*pre;
-  if(*start == NULL){
-    return;
-  }
-  ptr = *start;
-  while(ptr->next != NULL){
-    pre = ptr;
-    ptr = ptr->next;
-  }
-  if(pre == NULL){ //prev has one node
-      *start = NULL;
-  }else{ //prev has more than one
-      pre->next = NULL;
+
+void Backward(node *tail) {
+  node *new_node = tail;
+  while (new_node != NULL) {
+      printf("%d ", new_node->data);
+      new_node = new_node->prev;
   }
 }
-void deletenode(node** start, int value) {
-    if (*start == NULL) {
-        return; // Empty list, nothing to delete
-    }
 
-    node* ptr, *preptr;
-    ptr = *start;
-    preptr = NULL;
+node *New_Node(int val){
+    node *New_Node = (node *)malloc(sizeof(node));
+    New_Node->data = val;
+    New_Node->next = NULL;
+    New_Node->prev = NULL;
+    return New_Node;
+}
 
-    // Search for the node with the specified value
-    while (ptr != NULL && ptr->data != value) {
-        preptr = ptr;
-        ptr = ptr->next;
-    }
+void ADD(node **head){
+    int num;
+    scanf("%d", &num);
+    node *new_node = New_Node(num);
 
-    // If the value is not found, do nothing
-    if (ptr == NULL) {
+    if(*head == NULL){
+        *head = new_node;
         return;
     }
 
-    // If the node to be deleted is the first node
-    if (preptr == NULL) {
-        *start = ptr->next;
-    } else {
-        preptr->next = ptr->next;
+    node *new_node = *head;
+    while(new_node->next != NULL){
+        new_node = new_node->next;
+    }
+    new_node->next = new_node;
+    new_node->prev = new_node;
+}
+
+void SCH(node** head) {
+    node* new_node = *head;
+    int num;
+    scanf("%d", &num);
+
+    if (*head == NULL) {
+        printf("none\n");
+        return;
     }
 
-    free(ptr);
-}
+    while (new_node->next != NULL && new_node->data != num) {
+        new_node = new_node->next;
+    }
 
-
-void freelist(node* start){
-  node* ptr = start;
-  node* tmp;
-  while(ptr != NULL){
-    tmp = ptr->next;
-    free(ptr);
-    ptr = tmp;
-  }
-}
-int main(void) {
-  node* start = NULL;
-  int numofnode, data, value;
-  char choice;
-
-  // Read the number of nodes and their values
-  scanf(" %d", &numofnode);
-  for (int i = 0; i < numofnode; i++) {
-      scanf(" %d", &data);
-      insert_back(&start, data);
-  }
-  scanf(" %c", &choice);
-// ...
-
-while (1) {
-    if (choice == 'F') {
-        deletefirst(&start);
-    } else if (choice == 'L') {
-        deletelast(&start);
-    } else if (choice == 'N') {
-        scanf(" %d", &value);
-        if (value >= 1) {
-            deletenode(&start, value);
+    if (new_node->data == num) {
+        if (new_node->next == NULL && new_node->prev ==NULL) {
+            printf("NULL NULL\n");
+        } else if(new_node->next == NULL){
+            printf("%d NULL\n", new_node->prev->data);
+        } else if(new_node->prev == NULL){
+            printf("NULL %d\n", new_node->next->data);
+        } else{
+            printf("%d %d\n", new_node->prev->data, new_node->next->data);
         }
-    } else if (choice == 'E') {
-        break;
     } else {
-        continue;
+        printf("none\n");
+    }
+}
+
+void DEL(node **head) {
+    node *new_node = *head;
+    node *temp = NULL;
+    int num;
+    scanf("%d", &num);
+
+    if (new_node == NULL) {
+        return;
     }
 
-    // Read the next choice after performing the current operation
-    scanf(" %c", &choice);
+    if (new_node->data == num) {
+        *head = new_node->next;
+        if (new_node->next != NULL) {
+            new_node->next->prev = NULL;
+        }
+        return;
+    }
+
+    while (new_node != NULL && new_node->data != num) {
+        temp = new_node;
+        new_node = new_node->next;
+    }
+
+    if (new_node != NULL) {
+        temp->next = new_node->next;
+        if (new_node->next != NULL) {
+            new_node->next->prev = temp;
+        }
+    }
 }
 
 
-    // Print the remaining nodes
-    node* last = start;
-  if(start != NULL){
-    while (last != NULL) {
-        printf("%d ", last->data);
-        last = last->next;
 
+int main (void){
+  node *head = NULL;
+  node *tail = NULL;
+  char options [10];
+  do {
+    scanf("%s", options);
+    if(strcmp(options, "ADD") == 0) {
+      ADD(&head);
+    } else if (strcmp(options, "SCH") == 0){
+      SCH(&head);
+    } else if (strcmp(options, "DEL") == 0){
+      DEL(&head);
+    }else{
+      break;
     }
-  }else{
-    printf("none");
-  }
+  } while ( strcmp(options,"ADD") == 0 || strcmp(options,"SCH") == 0 || strcmp(options,"DEL") == 0);
 
-freelist(start);
-return 0;
-}
+    if(head == NULL){
+            printf("none\nnone");
+        }else{
+            node *new_node = head;
+            while(new_node->next != NULL){
+                new_node = new_node->next;
+            }
+            tail = new_node;
+            Forward(head);
+            printf("\n");
+            Backward(tail);
+        }
+        return 0;
+    }
